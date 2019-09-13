74739eba32330072b37aab18d9b0a1e8815dfb1c
root <aarokan@gmail.com>
2019-09-13 18:32:28 +0000

    Create ListMovies component

---
+++ src/ListMovies.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ListMovies extends Component {
+  render() {
+    console.log('Movies', this.props.movies);
+     return (
+          <h2></h2>
+      )
+  }
+}
+
+export default ListMovies;
